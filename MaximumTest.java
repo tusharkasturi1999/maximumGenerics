64ccd57cf6a5e6bb658841ed1d91f904276e6359
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-31 16:13:04 +0530

    Added Maximum for Generic Case  and its test cases

--- MaximumTest.java
+++ MaximumTest.java
@@ -2,42 +2,64 @@ package com.bridgelabz.generics;
 
 //UseCase 1 given 3 integers we have to find the maximum
 
-public class MaximumTest<String extends Comparable<String>> {
+public class MaximumTest<T extends Comparable<T>> 
+{
 	
-	String x,y,z;  //class variables
+	//class variables
+	 T x,y,z,w;  
 	
-	//parameterized construuctors
-	public MaximumTest(String x,String y,String z) {
+	//parameterized construuctors for 3 variables
+	public MaximumTest(T x,T y,T z) {
 		this.x=x;
 		this.y=y;
 		this.z=z;
 	}
 	
-	//to get the maximum value
-	 public String maximum()
-	 {
-		 return MaximumTest.maximum(x, y, z);
-	 }
-	 
-	//compare to function
-	public static <String extends Comparable<String>> String maximum(String x,String y,String z) 
-	{
-		String max=x;     //will assume x is maximum then compare with y and z
-		if(y.compareTo(max)>0) {
-			max=y;
-		}
-		if(z.compareTo(max)>0) {
-			max=z;
-		}
-		printMax(x,y,z,max);
-		return max;
+	//parameterized construuctors for 4 variables
+	public MaximumTest(T x,T y,T z,T w) {
+		this.x=x;
+		this.y=y;
+		this.z=z;
+		this.w=w;
 	}
 	
-	//to print the maximum value
-	public static <string> void printMax(string x,string y,string z,string max)
+	
+	//compare to function for 3 parameters
+	 public static <T extends Comparable<T>> T maximum(T x,T y,T z) {
+		 T max=x;     //will assume x is maximum then compare with y and z
+		 if(y.compareTo(max)>0) {
+			 max=y;
+		 }	
+		 if(z.compareTo(max)>0) {
+			 max=z;
+		 }
+		 printMax(x,y,z,max);
+		 return max;
+	 }
+	 //compare to function for 4 parameters
+	 public static <T extends Comparable<T>> T maximum(T x,T y,T z,T w) {
+		 T max=x;     //will assume x is maximum then compare with y and z
+		 if(y.compareTo(max)>0) {
+			 max=y;
+		 }
+		 if(z.compareTo(max)>0) {
+			 max=z;
+		 }
+		 if(w.compareTo(max)>0)
+		 {
+			 max=w;
+		 }
+		 printMax(x,y,z,w,max);
+		 return max;
+	 }
+	//to print the maximum value for 3 parameters
+	public static <T> void printMax(T x,T y,T z,T max)
 	{
 		System.out.printf("Max of %s, %s and %s is %s\n",x,y,z,max);
 	}
-	
-	
+	//to print the maximum value for 4 parameters
+	public static <T> void printMax(T x,T y,T z,T w,T max)
+	{
+		System.out.printf("Max of %s, %s, %s and %s is %s\n",x,y,z,w,max);
+	}
 }
--- Test.java
+++ Test.java
@@ -1,19 +1,31 @@
 package com.bridgelabz.generics;
 
+import java.util.*;
 
 public class Test {
 
 	public static void main(String[] args) {
-		//testcase 1
-		String xStr="pear",yStr="apple",zStr="orange";
-		new MaximumTest(xStr,yStr,zStr).maximum();
-		
-		//Testcase 2
-		String aStr="pear",bStr="apple",cStr="orange";
-		new MaximumTest(bStr,cStr,aStr).maximum();
-		
-		//Testcase 3
-		String pStr="pear",qStr="apple",rStr="orange";
-		new MaximumTest(rStr,pStr,qStr).maximum();
+		System.out.println("Enter the Number of parameters either 3 or 4");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if (n == 3) // when you want 3 parameters
+		{
+			String xStr = "melon", yStr = "apple", zStr = "grape";
+			new MaximumTest(xStr, yStr, zStr).maximum(xStr, yStr, zStr);
+			float xfloat = 100.6f, yfloat = 50.6f, zfloat = 70.9f;
+			new MaximumTest(xfloat, yfloat, zfloat).maximum(xfloat, yfloat, zfloat);
+			int xInt = 600, yInt = 50, zInt = 800;
+			new MaximumTest(xInt, yInt, zInt).maximum(xInt, yInt, zInt);
+		} else // when you want 4 parameters
+		{
+			String xStr = "melon", yStr = "apple", zStr = "grape", wstr = "pineapple";
+			new MaximumTest(xStr, yStr, zStr, wstr).maximum(xStr, yStr, zStr, wstr);
+			float xfloat = 100.6f, yfloat = 50.6f, zfloat = 670.9f, wfloat = 90.08f;
+			new MaximumTest(xfloat, yfloat, zfloat, wfloat).maximum(xfloat, yfloat, zfloat, wfloat);
+			int xInt = 900, yInt = 50, zInt = 700, wint = 677;
+			new MaximumTest(xInt, yInt, zInt, wint).maximum(xInt, yInt, zInt, wint);
+		}
+
 	}
+
 }
